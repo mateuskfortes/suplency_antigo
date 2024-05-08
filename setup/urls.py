89f659fa54779529bd8bd28e328b6643b2733ded
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-05-08 11:37:23 -0300

    c

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from suplency.views import Home
+from suplency.views import Home, sing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home, name='home'),
     path('accounts/', include('allauth.urls')),
+    path('tst', sing)
 ]
--- suplency/views.py
+++ suplency/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'suplency/home.html')
+
+def sing(request):
+    return render(request, 'socialaccount/signup.html')
