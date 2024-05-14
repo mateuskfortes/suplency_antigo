c656a53643122640a103b1b8184ab9a6cf405b9f
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-05-14 14:45:37 -0300

    hg

--- setup/settings.py
+++ setup/settings.py
@@ -112,12 +112,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
