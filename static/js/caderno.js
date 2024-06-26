1875a3e34ddbb19d549407aa28bf84fef099f5c3
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2024-06-25 23:49:15 -0300

    sdfg

--- static/js/caderno.js
+++ static/js/caderno.js
@@ -1,118 +1,302 @@
-let optionsButtons = document.querySelectorAll(".option-button");
-let advancedOptionButton = document.querySelectorAll(".adv-option-button");
-let fontName = document.getElementById("fontName");
-let fontSizeRef = document.getElementById("fontSize");
-let writingArea = document.getElementById("text-input");
-let linkButton = document.getElementById("createLink");
-let alignButtons = document.querySelectorAll(".align");
-let spacingButtons = document.querySelectorAll(".spacing");
-let formatButtons = document.querySelectorAll(".format");
-let scriptButtons = document.querySelectorAll(".script");
+const input_opcoes = document.querySelectorAll(".opcao-texto");
+const input_opcoes_avancado = document.querySelectorAll(".opcao-texto-avancado");
 
-//List of fontlist
-let fontList = [
+const input_fontName = document.getElementById("fontName");
+const input_fontSize = document.getElementById("fontSize");
+const caderno = document.getElementById("caderno");
+
+const input_de_alinhamento = document.querySelectorAll(".alinhamento");
+const input_de_espacamento = document.querySelectorAll(".spacing");
+const input_de_formatacao = document.querySelectorAll(".format");
+const input_de_script = document.querySelectorAll(".script");
+
+let ultima_posicao_cursor = null
+
+// LISTA DE FONTES
+const fontes = [
+  "Times New Roman",
   "Arial",
   "Verdana",
-  "Times New Roman",
   "Garamond",
   "Georgia",
   "Courier New",
   "cursive",
 ];
 
-//Initial Settings
+
 const initializer = () => {
-  //function calls for highlighting buttons
-  //No highlights for link, unlink,lists, undo,redo since they are one time operations
-  highlighter(alignButtons, true);
-  highlighter(spacingButtons, true);
-  highlighter(formatButtons, false);
-  highlighter(scriptButtons, true);
+  caderno.focus()
+  salvarPosicaoCursor()
+  createMutationObserver();
 
-  //create options for font names
-  fontList.map((value) => {
-    let option = document.createElement("option");
-    option.value = value;
-    option.innerHTML = value;
-    fontName.appendChild(option);
+  destacar(input_de_alinhamento);
+  destacar(input_de_script);
+  destacar(input_de_espacamento);
+  destacar(input_de_formatacao, false);
+
+  caderno.addEventListener("mouseup", salvarPosicaoCursor);
+
+  // CRIA AS OPÇÕES PARA O NOME DA FONTE
+  fontes.map((fontName) => {
+    let opcao = document.createElement("option");
+    opcao.value = fontName;
+    opcao.innerHTML = fontName;
+    input_fontName.appendChild(opcao);
   });
 
-  //fontSize allows only till 7
+  // CRIA AS OPÇÕES PARA O TAMANHO DA FONTE
   for (let i = 1; i <= 7; i++) {
-    let option = document.createElement("option");
-    option.value = i;
-    option.innerHTML = i;
-    fontSizeRef.appendChild(option);
+    let opcao = document.createElement("option");
+    opcao.value = i;
+    opcao.innerHTML = i;
+    input_fontSize.appendChild(opcao);
   }
-
-  //default size
-  fontSizeRef.value = 3;
+  // TAMANHO PADRAO DA FONTE
+  input_fontSize.value = 3;
 };
 
-//main logic
-const modifyText = (command, defaultUi, value) => {
-  //execCommand executes command on selected text
-  document.execCommand(command, defaultUi, value);
-  writingArea.focus()
-};
 
-//For basic operations which don't need value parameter
-optionsButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    modifyText(button.id, false, null);
-  });
-});
-
-//options that require value parameter (e.g colors, fonts)
-advancedOptionButton.forEach((button) => {
-  button.addEventListener("input", () => {
-    modifyText(button.id, false, button.value);
-  });
-});
-
-//link
-linkButton.addEventListener("click", () => {
-  let userLink = prompt("Enter a URL");
-  //if link has http then pass directly else add https
-  if (/http/i.test(userLink)) {
-    modifyText(linkButton.id, false, userLink);
-  } else {
-    userLink = "http://" + userLink;
-    modifyText(linkButton.id, false, userLink);
-  }
-});
-
-//Highlight clicked button
-const highlighter = (className, needsRemoval) => {
-  className.forEach((button) => {
-    button.addEventListener("click", () => {
-      //needsRemoval = true means only one button should be highlight and other would be normal
-      if (needsRemoval) {
-        let alreadyActive = false;
-
-        //If currently clicked button is already active
-        if (button.classList.contains("active")) {
-          alreadyActive = true;
-        }
-
-        //Remove highlight from other buttons
-        highlighterRemover(className);
-        if (!alreadyActive) {
-          //highlight clicked button
-          button.classList.add("active");
-        }
-      } else {
-        //if other buttons can be highlighted
-        button.classList.toggle("active");
+// DESTACA OS BOTOES PRECIONADOS
+function destacar(classeBotoes, unico=true) {
+  classeBotoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+      if (unico) {
+        removerDestaque(classeBotoes);
       }
+      botao.classList.toggle("active");
     });
   });
 };
 
-const highlighterRemover = (className) => {
-  className.forEach((button) => {
-    button.classList.remove("active");
+function removerDestaque(classeBotoes){
+  classeBotoes.forEach((botao) => {
+    botao.classList.remove("active");
   });
 };
 
-window.onload = initializer();
+window.onload = initializer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createMutationObserver() {
+  const observer = new MutationObserver(function(mutationsList, observer) {
+      for(let mutation of mutationsList) {
+          if (mutation.type === 'characterData') {
+              salvarPosicaoCursor()
+          }
+      }
+  });
+
+  // Observa mudanças no conteúdo do elemento contenteditable
+  observer.observe(caderno, { childList: true, subtree: true, characterData: true });
+}
+
+// Chama a função para criar o observador de mutações
+
+function salvarPosicaoCursor() {
+  const selecao = window.getSelection();
+  if (selecao.rangeCount > 0) {
+    const range = selecao.getRangeAt(0);
+    ultima_posicao_cursor = {
+      startContainer: range.startContainer,
+      startOffset: range.startOffset,
+      endContainer: range.endContainer,
+      endOffset: range.endOffset
+    };
+    console.log(ultima_posicao_cursor)
+  }
+}
+
+function setarPosicaoCursor() { 
+  if (ultima_posicao_cursor) {
+    const selecao = window.getSelection();
+    const range = document.createRange();
+    range.setStart(ultima_posicao_cursor.startContainer, ultima_posicao_cursor.startOffset);
+    range.setEnd(ultima_posicao_cursor.endContainer, ultima_posicao_cursor.endOffset);
+    selecao.removeAllRanges();
+    selecao.addRange(range);
+  }
+}
+
+let lastCursorPosition = null;
+
+function saveCursorPosition() {
+  const selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    lastCursorPosition = range.endOffset; // Armazena o offset de onde o cursor está no final da seleção
+  }
+}
+
+function restoreCursorPosition() {
+  const selection = window.getSelection();
+  if (lastCursorPosition !== null) {
+    const range = document.createRange();
+    range.setStart(selection.anchorNode, lastCursorPosition);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+}
+
+// FUNÇÃO PRINCIPAL
+function modifyText(command, defaultUi, value) {
+  setarPosicaoCursor()
+  document.execCommand(command, defaultUi, value);
+  
+};
+
+// OPÇÕES QUE NÃO PRECISAM DE PARÂMETRO
+input_opcoes.forEach((botao) => {
+  botao.addEventListener("click", () => {
+    if (botao == document.getElementById('insertOrderedList')) {
+      saveCursorPosition()
+      modifyText(botao.id, false, null);
+      restoreCursorPosition()
+    } else {
+      modifyText(botao.id, false, null);
+    }
+  });
+});
+
+// OPÇÕES QUE PRECISAM DE PARÂMETRO
+input_opcoes_avancado.forEach((botao) => {
+  botao.addEventListener("change", () => {
+    modifyText(botao.id, false, botao.value);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+caderno.addEventListener("click", updateOptions);
+
+function updateOptions() {
+  updateFontSize()
+  updateNegrito()
+}
+
+function updateFontSize() {
+  const estilo = estiloLocalSelecionado()
+  if (estilo) {
+    const fontSize = parseInt(estilo.fontSize)
+    var selectedFontSize = null
+    switch (fontSize) {
+      case 10:
+        selectedFontSize = 1;
+        break;
+      case 13:
+        selectedFontSize = 2;
+        break;
+      case 16:
+        selectedFontSize = 3;
+        break;
+      case 18:
+        selectedFontSize = 4;
+        break;
+      case 24:
+        selectedFontSize = 5;
+        break;
+      case 32:
+        selectedFontSize = 6;
+        break;
+      case 48:
+        selectedFontSize = 7;
+        break;
+      default:
+        selectedFontSize = 1
+        break;
+                                  
+    }
+    input_fontSize.value = selectedFontSize;
+  }
+}
+
+function updateNegrito() {
+  const estilo = estiloLocalSelecionado()
+  if (estilo) {
+    if (estilo.fontWeight === 'bold') {
+      
+    }
+  }
+}
+
+
+// RETORNA O OBJETO SELECIONADO
+function estiloLocalSelecionado() {
+  const selecao = window.getSelection();
+  if (selecao.rangeCount > 0) {
+    const range = selecao.getRangeAt(0);
+    const elemento_em_foco = range.startContainer.parentElement;
+    const estilo = window.getComputedStyle(elemento_em_foco);
+    return estilo;
+  }
+  return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- static/js/pomodoro.js
+++ static/js/pomodoro.js
@@ -6,24 +6,36 @@ const contador = document.getElementById('timer')
 const input_foco = document.getElementById('tp_foco')
 const input_pausa = document.getElementById('tp_pausa')
 const input_pausa_longa = document.getElementById('tp_pausa_longa')
-const input_ate_pl = document.getElementById('focos_ate_pausa_longa')
+const input_ate_pausa_longa = document.getElementById('focos_ate_pausa_longa')
 
 let tempo_total_foco = 0
 let tempo_total_pausa = 0
 let tempo_total_pausa_longa = 0
-let ate_pl = 1
+let ate_pausa_longa = 1
 let ligado = false
 let timer = 0
 let tempo = 0
-let i = ate_pl
+let sequencia = []
+let sequencia_atual=''
 
 start.addEventListener('click', () => {
     tempo_total_foco = input_foco.value * 60
     tempo_total_pausa = input_pausa.value * 60
     tempo_total_pausa_longa = input_pausa_longa.value * 60
-    ate_pl = input_ate_pl.value * 2
-    if (!ligado && input_foco.value > 0 && input_pausa.value > 0 && input_pausa_longa.value > 0 && input_ate_pl.value > 0) pomodoro()
-    else parar_timer()
+    ate_pausa_longa = input_ate_pausa_longa.value
+    if (!ligado
+    && input_foco.value > 0
+    && input_pausa.value > 0
+    && input_pausa_longa.value > 0 
+    && input_ate_pausa_longa.value > 0) 
+    {
+        preencher_sequencia()
+        pomodoro()
+    }
+    else
+    {
+        parar_timer()
+    }
 })
 
 input_foco.addEventListener('input', () => {
@@ -34,10 +46,14 @@ input_foco.addEventListener('input', () => {
 async function pomodoro() {
     ligado = true
     while (ligado) {
-        for (i = 0; i < ate_pl; i++) {
-            if (i % 2 == 0) tempo = tempo_total_foco
-            else if (i != ate_pl - 1) tempo = tempo_total_pausa
-            else tempo = tempo_total_pausa_longa
+        for (let momento of sequencia) {
+            sequencia_atual = momento
+            if (!ligado) break
+
+            else if (momento == 'foco') tempo = tempo_total_foco
+            else if (momento == 'pausa') tempo = tempo_total_pausa
+            else if (momento == 'pausa_longa') tempo = tempo_total_pausa_longa
+
             await new Promise(resolve => timer = setInterval(() => {
                 tempo--
                 mostrar_timer()
@@ -51,17 +67,26 @@ async function pomodoro() {
     }
 }
 
+function preencher_sequencia() {
+    sequencia = []
+    for (i=0; i <= ate_pausa_longa; i++) {
+        sequencia.push('foco')
+        if (i != ate_pausa_longa) sequencia.push('pausa')
+        else sequencia.push('pausa_longa')
+    }
+    console.log(sequencia)
+}
+
 function mostrar_timer() {
     var minutos = Math.floor(tempo / 60);
     var segundos = tempo % 60;
     var mostrar_tempo = `${minutos < 10 ? '0' : ''}${minutos}:${segundos < 10 ? '0' : ''}${segundos}`
-    contador.textContent = `${mostrar_tempo} ${i % 2 == 0 && i != ate_pl ? 'foco' : i % 2 != 0 && i != ate_pl - 1 ? 'pausa' : i == ate_pl - 1 ? 'pausa longa' : ''}`
+    contador.textContent = `${mostrar_tempo} ${sequencia_atual}`
 }
 
-function parar_timer() {
+function parar_timer(tempo_final = 0) {
     clearInterval(timer)
     ligado = false
-    i = ate_pl
-    tempo = 0
+    tempo = tempo_final
     mostrar_timer()
 }
